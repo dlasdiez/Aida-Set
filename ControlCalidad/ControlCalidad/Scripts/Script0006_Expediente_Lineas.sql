3accaa6986174477ff2660a193909ffe56da8c64
WhiteOso <joaquim.calero@gmail.com>
2015-09-09 23:19:42 +0200

    BD
    Expediente Lineas con Indice sobre columna Expediente

---
+++ ControlCalidad/ControlCalidad/Scripts/Script0006_Expediente_Lineas.sql
@@ -0,0 +1,15 @@
+USE Metalcaucho;
+
+CREATE TABLE IF NOT EXISTS `expediente_lineas` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Id_expediente` bigint(20) NOT NULL,
+  `fecha_creacion` datetime NULL,
+  `fecha_modificacion` datetime NULL,
+  `usuario_creacion_id` bigint(20),
+  `proveedor_id` bigint(20),
+  `proveedor_nombre` varchar(255),
+  `referencia_proveedor` varchar(255),
+  PRIMARY KEY (`Id`,`Id_expediente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+ALTER TABLE  `metalcaucho`.`expediente_lineas` ADD INDEX  `Expediente` (  `Id_expediente` );
